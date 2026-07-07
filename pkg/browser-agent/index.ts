fcd7ff6fbec300e827a66a95b7421e589c269d22
zavx0z <zavx0z@yahoo.com>
2026-07-07 14:06:34 +0300

    Export browser agent provider API

--- pkg/browser-agent/index.ts
+++ pkg/browser-agent/index.ts
@@ -1,10 +1,13 @@
 export {
   createBrowserAgentRuntime,
+  DEFAULT_DEEPSEEK_URL_CONTAINS,
   DEFAULT_QWEN_URL_CONTAINS,
 } from "./src/runtime.ts"
 
 export type {
   BrowserAgentHost,
   BrowserAgentJsonObject,
+  BrowserAgentProvider,
+  BrowserAgentProviderId,
   BrowserAgentRuntime,
 } from "./src/types.ts"
