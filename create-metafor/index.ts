e9749a7cf55bfb865694590149b9c0ce147c3acb
zavx0z <zavx0z@yahoo.com>
2026-08-23 06:33:54 +0300

    refactor(workspace): wire create-metafor internal packages

--- create-metafor/index.ts
+++ create-metafor/index.ts
@@ -1,5 +1,5 @@
-export {MetaFor} from "../metafor.ts"
-export {validateMatter} from "../matter.ts"
+export {MetaFor} from "./dsl/metafor.ts"
+export {validateMatter} from "./dsl/matter.ts"
 export {createMetaforSqliteFixture} from "./fixture/index.ts"
 export {
   MetaCreatePatchError,
